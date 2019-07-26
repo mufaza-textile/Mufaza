372878ebecaddad60bb51ab1f3fa64da35a54f06
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2019-07-26 22:09:08 +0530

    new components created
    working !!
    app.module eddited & updated

--- Mufaza/Mufaza-Website/src/app/app.module.ts
+++ Mufaza/Mufaza-Website/src/app/app.module.ts
@@ -5,12 +5,23 @@ import { AngularFireDatabaseModule} from '@angular/fire/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+
+import { RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const config = {
   apiKey: "AIzaSyDXeOHxnq90j-DE3Re42X-74N9wbvxF8qM",
@@ -24,7 +35,17 @@ const config = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +54,17 @@ const config = {
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
     AngularFireStorageModule, // storage
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'shopping-cart', component: ShoppingCartComponent},
+      {path: 'check-out', component: CheckOutComponent},
+      {path: 'order-success', component: OrderSuccessComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'admin/products', component: AdminProductsComponent},
+      {path: 'admin/orders', component: AdminOrdersComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
