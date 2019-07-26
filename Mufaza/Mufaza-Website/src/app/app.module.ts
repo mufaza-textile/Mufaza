4b0d46f701da588dcceb4b8c06572868dedebb51
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2019-07-26 20:20:05 +0530

    version updated
    firebase working

--- Mufaza/Mufaza-Website/src/app/app.module.ts
+++ Mufaza/Mufaza-Website/src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule} from 'angularfire2/database';
-import { AngularFireAuthModule} from 'angularfire2/auth';
+
+import { AngularFireDatabaseModule} from '@angular/fire/database';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyDXeOHxnq90j-DE3Re42X-74N9wbvxF8qM",
+  authDomain: "mufaza-textile.firebaseapp.com",
+  databaseURL: "https://mufaza-textile.firebaseio.com",
+  projectId: "mufaza-textile",
+  storageBucket: "mufaza-textile.appspot.com",
+  messagingSenderId: "183434512969",
+  appId: "1:183434512969:web:bb246ce986625cbb"
+};
 
 @NgModule({
   declarations: [
@@ -14,9 +29,10 @@ import { environment } from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
