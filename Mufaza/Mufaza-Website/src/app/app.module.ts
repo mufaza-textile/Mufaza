ed6581fac1ec0b2b948b1585dc8690b1d28dcec5
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2019-07-30 16:55:47 +0530

    Order main components

--- Mufaza/Mufaza-Website/src/app/app.module.ts
+++ Mufaza/Mufaza-Website/src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 
-import { AppRoutingModule } from './app-routing.module';
+import { environment } from './../environments/environment';
+import { AdminModule } from './admin/admin.module';
+import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ProductsComponent } from './shopping/components/products/products.component';
+import { ShoppingModule } from './shopping/shopping.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
+    AdminModule,
+    ShoppingModule,
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    RouterModule.forRoot([
+      { path: '', component: ProductsComponent },
+      { path: 'login', component: LoginComponent },
+    ])    
   ],
-  providers: [],
+  providers: [
+    AdminAuthGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
