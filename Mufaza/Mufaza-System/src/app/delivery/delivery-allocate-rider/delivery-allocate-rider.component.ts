02e3aee35312be3710bdc6e11942ce83290d454e
charitha98 <charithasovis@gmail.com>
2019-08-24 23:24:53 +0530

    added shared file

--- Mufaza/Mufaza-System/src/app/delivery/delivery-allocate-rider/delivery-allocate-rider.component.ts
+++ Mufaza/Mufaza-System/src/app/delivery/delivery-allocate-rider/delivery-allocate-rider.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DeliveryService } from "../../shared/delivery-add.service";
+import { NotifcationService } from "../../shared/notifcation.service";
 
 @Component({
   selector: 'app-delivery-allocate-rider',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryAllocateRiderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: DeliveryService,
+    private notificationService : NotifcationService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      this.service.getDelivery();
+    }
+  
+  
+  
+    onClear() {
+      this.service.form.reset();
+      this.service.initializeFormGroup();
+  
+    }
+  
+    onSubmit(){
+      if(this.service.form.valid){
+        this.service.insertDelivery(this.service.form.value)
+        this.service.form.reset();
+        this.service.initializeFormGroup();
+        this.notificationService.success(':: Submitted Succesfully' );
+  
+      }
+  
+    }
 
 }
