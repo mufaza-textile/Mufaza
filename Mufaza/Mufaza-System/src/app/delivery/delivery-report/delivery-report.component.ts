c5b4d2cdc6c43de4f22aca3a0738e7d80b797a9e
charitha98 <charithasovis@gmail.com>
2019-08-25 11:46:23 +0530

    updated component.ts files

--- Mufaza/Mufaza-System/src/app/delivery/delivery-report/delivery-report.component.ts
+++ Mufaza/Mufaza-System/src/app/delivery/delivery-report/delivery-report.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { DeliveryService } from 'src/app/shared/delivery-add.service';
 
 @Component({
   selector: 'app-delivery-report',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryReportComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service : DeliveryService) { }
+ 
+  listData: MatTableDataSource<any>;
+  
+  displayedColumns: string[] =[`riderId`,`orderId`,`cusName`,`deliveryAddress`,`deliveryDate`];
+  
+      @ViewChild(MatSort,{static: true}) sort: MatSort;
+      @ViewChild(MatPaginator,{static: true}) paginator: MatPaginator;
 
   ngOnInit() {
+    this.service.getDelivery().subscribe(
+      list => {
+        let array = list.map(item =>{
+          return {
+            $key: item.key,
+            ...item.payload.val()
+          };
+        });
+        this.listData = new MatTableDataSource(array);
+        this.listData.sort = this.sort;
+        this.listData.paginator = this.paginator;
+      }
+    );
   }
-
 }
+
