0c4e606d7f6d11071e61f52c0d957e2d8bf0dcd3
Savandi <savandi98@gmail.com>
2019-08-29 17:19:08 +0530

    shopping-cart-summary component

--- Mufaza/Mufaza-Website/src/app/shopping/components/shopping-cart-summary/shopping-cart-summary.component.ts
+++ Mufaza/Mufaza-Website/src/app/shopping/components/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -2,7 +2,7 @@ import { ShoppingCart } from '../../../shared/models/shopping-cart';
 import { Component, OnInit, Input } from '@angular/core';
 import {PromotionService} from 'shared/services/promotion.service';
 import {Promotions} from 'shared/models/Promotions';
-import { Observable } from 'rxjs/Observable';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'shopping-cart-summary',
@@ -10,21 +10,21 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./shopping-cart-summary.component.css']
 })
 export class ShoppingCartSummaryComponent implements OnInit{
+ 
   @Input('cart') cart: ShoppingCart;
   promocode: string;
   newprice: any;
   promo: boolean = false;
   error:boolean= false;
 
-  promotions: Promotions[];
-constructor(public promotionService: PromotionService){
+  public promotions: FirebaseListObservable<Promotions[]>;
+  constructor(public promotionService: PromotionService){
 
 }
 
 ngOnInit(){
- 
+  this.promotions = this.promotionService.getPromoList({});
 }
-
 
   promotion(promocode: string){
     this.promocode = promocode;
