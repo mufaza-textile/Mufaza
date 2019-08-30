6baad751eccf6cc52e706ab3c518a3ae45b409c0
Praveen Nithiyananda <n.praveen28j@gmail.com>
2019-08-30 15:34:56 +0530

    small

--- Mufaza/Mufaza-System/src/app/shared/register.service.ts
+++ Mufaza/Mufaza-System/src/app/shared/register.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn } from '@angular/forms';
 import { AngularFireDatabase,AngularFireList } from "angularfire2/database";
 import { __values } from 'tslib';
 
@@ -25,13 +25,27 @@ export class RegisterService {
   form : FormGroup = new FormGroup({
 
     $key : new FormControl(null),
-    empId : new FormControl('',[Validators.required,Validators.maxLength(5)]),
+    empId : new FormControl('',[Validators.required,Validators.maxLength(5),this.alphaNumeric('abc')]),
     position : new FormControl('',[Validators.required]),
     password : new FormControl('',[Validators.required,Validators.minLength(5)])
   
 
 
   });
+
+  alphaNumeric(allowedPhrase: string): ValidatorFn {
+    return (c: FormControl): { [key: string]: boolean } | null => {
+      if (c.value) {
+        allowedPhrase = allowedPhrase ? allowedPhrase : '';
+        let regEx = new RegExp(/^[a-zA-Z0-9]*$/);
+        if (!regEx.test(c.value.replace(allowedPhrase.toUpperCase(), '').replace(allowedPhrase.toLowerCase(), ''))) {
+          return { 'alphanumeric': true };
+        }
+      }
+      return null;
+    };
+  };
+
 
   initializeFormGroup(){
 
