acc6b6f888e706b7d114f603b632dbf54bd55d0c
sameeratse <sameeratse@gmail.com>
2019-09-01 15:02:59 +0530

    tailring

--- Mufaza/Mufaza-System/src/app/shared/tailoring.service.ts
+++ Mufaza/Mufaza-System/src/app/shared/tailoring.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { FormGroup, FormControl, Validators} from "@angular/forms"
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Injectable()
 export class TailoringService {
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) { }
+
+  tailoringList: AngularFireList<any>;
 
   form: FormGroup = new FormGroup({
     $key: new FormControl(null),
@@ -36,4 +39,45 @@ export class TailoringService {
       deliveryDate: ''
     });
   }
+
+  getTailroings(){
+    this.tailoringList =this.firebase.list('tailorings');
+    return this.tailoringList.snapshotChanges();
+  }
+
+  insertTailoring(tailoring){
+    this.tailoringList.push({
+      orderID: tailoring.orderID,
+      customerID: tailoring.customerID,
+      username:  tailoring.username,
+      chest: tailoring.chest,
+      shoulder: tailoring.shoulder,
+      arms: tailoring.arms,
+      frontNeck: tailoring.frontNeck,
+      backNeck: tailoring.backNeck,
+      length: tailoring.length,
+      deliveryDate: tailoring.deliveryDate
+    });
+  }
+
+  updateTailoring(tailoring){
+    this.tailoringList.update(tailoring.$key,
+      {
+        orderID: tailoring.orderID,
+        customerID: tailoring.customerID,
+        username:  tailoring.username,
+        chest: tailoring.chest,
+        shoulder: tailoring.shoulder,
+        arms: tailoring.arms,
+        frontNeck: tailoring.frontNeck,
+        backNeck: tailoring.backNeck,
+        length: tailoring.length,
+        deliveryDate: tailoring.deliveryDate
+      });
+  }
+
+  deleteTailoring($key: string){
+    this.tailoringList.remove($key);
+  }
+
 }
