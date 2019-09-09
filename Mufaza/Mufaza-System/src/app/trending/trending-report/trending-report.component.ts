fa535ecb2aefa87b4ca846d7cfb5aeebb196049b
Dhanuja <dhanujaprathapa@gmail.com>
2019-09-10 00:34:21 +0530

    report title

--- Mufaza/Mufaza-System/src/app/trending/trending-report/trending-report.component.ts
+++ Mufaza/Mufaza-System/src/app/trending/trending-report/trending-report.component.ts
@@ -40,7 +40,7 @@ export class TrendingReportComponent implements OnInit {
   }
 
   print(){
-    var data = document.getElementById('table');  
+    var data = document.getElementById("report");  
     html2canvas(data).then(canvas => {  
       // Few necessary setting options  
       var imgWidth = 208;   
@@ -52,7 +52,7 @@ export class TrendingReportComponent implements OnInit {
       let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
       var position = 0;  
       pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-      pdf.save('MYPdf.pdf'); // Generated PDF   
+      pdf.save('trending.pdf'); // Generated PDF   
     });  
   }
 
