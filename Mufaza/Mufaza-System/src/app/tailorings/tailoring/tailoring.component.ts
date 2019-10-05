bef8b5c7f19cf04b27396fdf215e624ad3ea9d70
sameeratse <sameeratse@gmail.com>
2019-10-05 17:20:18 +0530

    add

--- Mufaza/Mufaza-System/src/app/tailorings/tailoring/tailoring.component.ts
+++ Mufaza/Mufaza-System/src/app/tailorings/tailoring/tailoring.component.ts
@@ -12,42 +12,43 @@ export class TailoringComponent implements OnInit {
   constructor(private service: TailoringService) { }
 
   chests =[
-    { id: 3, value: "10"},
+    { id: 1, value: "10"},
     { id: 2, value: "12"},
     { id: 3, value: "14"}
   ];
 
   shoulders =[
-    { id: 3, value: "10"},
+    { id: 1, value: "10"},
     { id: 2, value: "12"},
     { id: 3, value: "14"}
   ];
 
   armss =[
-    { id: 3, value: "10"},
+    { id: 1, value: "10"},
     { id: 2, value: "12"},
     { id: 3, value: "14"}
   ];
 
   frontNecks =[
-    { id: 3, value: "10"},
+    { id: 1, value: "10"},
     { id: 2, value: "12"},
     { id: 3, value: "14"}
   ];
 
   backNecks =[
-    { id: 3, value: "10"},
+    { id: 1, value: "10"},
     { id: 2, value: "12"},
     { id: 3, value: "14"}
   ];
 
   lengths =[
-    { id: 3, value: "10"},
+    { id: 1, value: "10"},
     { id: 2, value: "12"},
     { id: 3, value: "14"}
   ];
 
   ngOnInit() {
+    this.service.getTailorings();
   }
 
   onClear(){
@@ -55,4 +56,11 @@ export class TailoringComponent implements OnInit {
     this.service.initializeFormGroup();
   }
 
+  onSubmit(){
+    if (this.service.form.valid){
+      this.service.insertTailoring(this.service.form.value);
+      this.service.form.reset();
+      this.service.initializeFormGroup();
+    }
+  }
 }
